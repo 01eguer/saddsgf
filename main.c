537948cc78395aaf2b1e05fc43e6dcfd5e9a0e19
Your Name <you@example.com>
2025-02-20 17:14:33 +0000

    implemenar resta

--- main.c
+++ main.c
@@ -16,6 +16,18 @@ int main() {
     printf("The sum of %d and %d is %d\n", num1, num2, sum);
 
 
+    // Prompt the user for input
+    printf("RESTA: Enter the first number: ");
+    scanf("%d", &num1);
+
+    printf("RESTA: Enter the second number: ");
+    scanf("%d", &num2);
+
+    // Calculate the sum
+    sum = num1 - num2;
+
+    printf("The sum of %d and %d is %d\n", num1, num2, sum);
+
 
     // Display the result
 
